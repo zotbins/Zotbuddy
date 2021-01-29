90a6ddabdb51fd33eaec19e43ef13848268dc577
jjchung3 <joejoonchung@gmail.com>
2021-01-28 20:40:35 -0800

    chore(): chore...

--- App.js
+++ App.js
@@ -1,14 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
+import firebase from 'firebase'
+import { AppLoading } from 'expo'
 
+import { firebaseConfig } from './config'
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+async function loadResourcesAsync() {
+  await Promise.all([
+    Asset.loadAsync([
+      require('./assets/images/robot-dev.png'),
+      require('./assets/images/robot-prod.png'),
+    ]),
+    Font.loadAsync({
+      // This is the font that we are using for our tab bar
+      ...Ionicons.font,
+      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+      // remove this if you are not using it in your app
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+    }),
+  ])
+}
 
 const App = props => {
-  return (
-    <View>
-      <Text>Placeholder</Text>
-    </View>
-  )
+  const [isLoadingComplete, setLoadingComplete] = useState(false)
+
+  async function loadResourcesAsync() {
+    await Promise.all([
+      Asset.loadAsync([
+        require('./assets/images/robot-dev.png'),
+        require('./assets/images/robot-prod.png'),
+      ]),
+      Font.loadAsync({
+        // This is the font that we are using for our tab bar
+        ...Ionicons.font,
+        // We include SpaceMono because we use it in HomeScreen.js. Feel free to
+        // remove this if you are not using it in your app
+        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+      }),
+    ])
+  }
+  
+  function handleLoadingError(error) {
+    // In this case, you might want to report the error to your error reporting
+    // service, for example Sentry
+    console.warn(error)
+  }
+  
+  function handleFinishLoading(setLoadingComplete) {
+    setLoadingComplete(true)
+  }
+  
+  if (!isLoadingComplete && !props.skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={() => handleFinishLoading(setLoadingComplete)}
+      />
+    )
+  } else {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        <AppNavigator />
+      </View>
+    )
+  }
 }
 
 export default App 
