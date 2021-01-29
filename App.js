35123a7f950958cbb5d418699c8d6b8f33d89082
jjchung3 <joejoonchung@gmail.com>
2021-01-28 20:18:06 -0800

    chore(): porting

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+
+const App = props => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Placeholder</Text>
     </View>
-  );
+  )
 }
+
+export default App 
 
 const styles = StyleSheet.create({
   container: {
