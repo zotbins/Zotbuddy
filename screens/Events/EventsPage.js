6a7bc54867fd3fdfe13f4994a510d95039973f0a
DevPopat <popatd@uci.edu>
2021-04-23 09:47:30 -0700

    feat(): update events page and fix profile page

--- screens/Events/EventsPage.js
+++ screens/Events/EventsPage.js
@@ -1,29 +1,39 @@
-import React, { useState } from 'react'
-import { 
-  Container, 
-  Header, 
-  Content, 
-  Card, 
-  CardItem, 
-  Thumbnail, 
-  Text, 
-  Button, 
-  Icon, 
-  Left, 
-  Body, 
-  Right,
-  Title,
-  Accordion,
-} from 'native-base'
+import React, { useState, useEffect } from 'react'
+import EventsForm from "./EventsForm"
+import * as firebase from 'firebase'
+import 'firebase/firestore'
 
 const EventsPage = props => {
-  return (
-    <Card>
-      <Text>
-        events
-      </Text>
-    </Card>
-  )
+  const [arr, setArr] = useState([])
+  const getEvents = async (_) => {
+    let arr = []
+    const db = firebase.firestore()
+    const query = db.collection("event").orderBy('startDate', 'asc')
+    await query.get().then((querySnapshot) => {
+        querySnapshot.forEach((userDoc) => {
+            arr.push({...userDoc.data(), key: arr.length + 1})
+        })
+    })
+    console.log(arr)
+    setArr(arr)
+  }
+  useEffect(() => {
+     async function cover(){
+        getEvents()
+     }
+     cover()
+    }, [])
+  
+  if (arr.length == 0) {
+    return<></>
+      
+  }
+  else{
+    return (
+      <EventsForm blood = {arr}/>
+    )
+  }
+    
 }
 
 export default EventsPage;
