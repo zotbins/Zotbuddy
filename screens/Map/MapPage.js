dda1121685ac07b5ed8fd9984782fda7fe48017c
tin-the-element <trungtin.luu@gmail.com>
2021-04-27 13:09:42 -0700

    feat: added social media page

--- screens/Map/MapPage.js
+++ screens/Map/MapPage.js
@@ -21,16 +21,16 @@ import {
   CardItem
 } from 'native-base'           
 
-import MapboxGL from "@react-native-mapbox-gl/maps";
+// import MapboxGL from "@react-native-mapbox-gl/maps";
 
-MapboxGL.setAccessToken("pk.eyJ1Ijoiem90Ymluc3Rlc3QiLCJhIjoiY2tud3B0OTYxMGV2YTJzbzhxbGF1NHN2MyJ9.3xw5Mq1Rlk-5ioy4HHhtvg");
+// MapboxGL.setAccessToken("pk.eyJ1Ijoiem90Ymluc3Rlc3QiLCJhIjoiY2tud3B0OTYxMGV2YTJzbzhxbGF1NHN2MyJ9.3xw5Mq1Rlk-5ioy4HHhtvg");
 
 const MapPage = props => {
     return (
       <View>
-          <Text>hi</Text>
+          <Text>Map</Text>
       </View>
     )
   }
 
-  export default MapPage;
+export default MapPage;
