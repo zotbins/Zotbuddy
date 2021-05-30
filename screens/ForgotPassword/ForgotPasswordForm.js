66ecf5feda279971ea82ba7feb6848c66fe0066b
Bhavani Panda <33610242+psgbhavani@users.noreply.github.com>
2021-05-30 13:32:04 -0700

    style: forgot password page restyled (mid fidelity)

--- screens/ForgotPassword/ForgotPasswordForm.js
+++ screens/ForgotPassword/ForgotPasswordForm.js
@@ -4,8 +4,9 @@ import { useForm } from 'react-hook-form'
 import Constants from 'expo-constants'
 import * as firebase from 'firebase'
 import 'firebase/firestore'
-import zotBinsLogo from '../../assets/images/ZotBins_Icon.png'
-import { Icon, Button } from 'native-base'
+//import zotBinsLogo from '../../assets/images/ZotBins_Icon.png'
+import zotZeroLogo from '../../assets/images/Logo-ZotZero-Horizontal-02.svg'
+import { Icon, Button, Card, CardItem } from 'native-base'
 
 
 const ForgotPasswordForm =  props  => {
@@ -29,96 +30,99 @@ const ForgotPasswordForm =  props  => {
   return (
     <View style={styles.container}>
       <Button iconLeft large transparent onPress={backToLogin} style={styles.arrowButton}>
-        <Icon name='arrow-back' color='#3b5998' style={styles.backArrow}/>
+        <Icon name='arrow-back' onPress={backToLogin} style={styles.backArrow}/>
       </Button>
-      <Image style={styles.zotBinsLogo} source={zotBinsLogo}/>
       <Text style={styles.label1}>Forgot Password?</Text>
 
-      <Text style={styles.label2}>No worries! Enter the email associated with your account to reset your password.</Text>
+      <Card style={styles.card}>
+          <Text style={styles.label2}>No worries! Enter your registered email below to reset your password.</Text>
 
-      <Text style={styles.label3}>Email</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={text => setValue('email', text, true)}
-      />
-      {errors.email && <Text>This is required.</Text>}
+          <Text style={styles.label3}>Email</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setValue('email', text, true)}
+          />
+          {errors.email && <Text>This is required.</Text>}
 
-      <Text style={styles.label}> </Text>
+          <Text style={styles.label}> </Text>
 
-      <Button rounded onPress={passwordReset} style={styles.button}>
-         <Text style={styles.label4}>Send</Text>
-      </Button>
+          <Button rounded onPress={passwordReset} style={styles.button}>
+             <Text style={styles.label4}>Send Email</Text>
+          </Button>
+      </Card>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   label1: {
-    color: '#000000',
-    fontSize: 24,
-    marginTop: 70,
+    color: '#0064A4',
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginTop: 125,
+    marginBottom: 15,
     alignSelf: 'center'
   },
   label2: {
-    color: '#000000',
+    color: '#6AA2B8',
     marginTop: 20,
-    marginLeft: 20,
-    marginRight: 20,
+    marginLeft: 25,
+    marginRight: 10,
+    marginBottom: 5,
     fontSize: 16,
     alignSelf: 'center',
-    textAlign: 'center'
+    textAlign: 'left'
   },
   label3: {
-    color: '#000000',
+    color: '#555759',
     marginTop: 20,
-    marginLeft: 20,
     marginRight: 20,
-    fontSize: 16,
-    marginLeft: 32
+    fontSize: 14,
+    marginLeft: 25
   },
   label4: {
-    color: '#000000',
+    color: 'white',
     fontSize: 16,
   },
   button :{
     marginTop: 20,
     alignSelf: 'center',
-    width: 126,
-    height: 34,
-    color: 'black',
-    backgroundColor: '#C4C4C4',
+    width: '65%',
+    height: 38,
+    marginBottom: 25,
+    backgroundColor: '#0064A4',
     justifyContent: 'center'
   },
   container: {
     flex: 1,
-    paddingTop: Constants.statusBarHeight,
-    padding: 8,
+    //paddingTop: Constants.statusBarHeight,
+    padding: 15,
     width: '100%',
-    backgroundColor: 'white'
+    backgroundColor: '#F4F4F4'
   },
   input: {
-    backgroundColor: '#F3F3F3',
+    //backgroundColor: '#F3F3F3',
     color: '#4F4F4F',
     height: 40,
-    marginLeft: 28,
-    marginRight: 27,
+    marginLeft: 20,
+    marginRight: 20,
     padding: 10,
     borderRadius: 4,
-  },
-  zotBinsLogo: {
-      height: 157,
-      resizeMode: 'contain',
-      alignSelf: 'center',
-      marginTop: 40,
-      marginLeft: 20
+    borderColor: '#555759',
+    borderWidth: 1
   },
   backArrow: {
       width: 145,
-      fontSize: 50,
-      color: 'black',
+      fontSize: 40,
+      color: '#0064A4',
+      marginLeft: 2
   },
   arrowButton: {
-      width: '20%',
+      width: '10%',
+  },
+  card: {
+    marginLeft: 10,
+    marginRight: 10
   }
 })
 
