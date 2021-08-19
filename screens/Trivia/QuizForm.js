07af9c46db60f099928b71cfe93bc237e03da83e
jjchung3 <joejoonchung@gmail.com>
2021-08-19 15:13:55 -0700

    feat(quiz): fix quiz fetching

--- screens/Trivia/QuizForm.js
+++ screens/Trivia/QuizForm.js
@@ -1,15 +1,8 @@
 import React, { useState } from 'react'
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-} from 'react-native'
-import {
-    Text,
-    Card,
-} from 'native-base'
+import { StyleSheet, View, TouchableOpacity } from 'react-native'
+import { Text, Card } from 'native-base'
 
-const progressBarPercents = ['20%', '40%', '60%', '80%', '100%'];
+const progressBarPercents = ['20%', '40%', '60%', '80%', '100%']
 
 const QuizForm = (props) => {
   const {
@@ -19,198 +12,240 @@ const QuizForm = (props) => {
     previousQuestion,
     saveChoice,
     onSubmit,
-    chosenAnswer
   } = props
 
   return (
     <View style={styles.container}>
-        <View style={styles.header}>
-            <Text style={styles.headerText}>Title of Daily Trivia Quiz!</Text>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Title of Daily Trivia Quiz!</Text>
+      </View>
+      <View>
+        <View style={styles.progressBar}>
+          <View
+            style={styles.absoluteFill}
+            width={progressBarPercents[currentIndex]}
+          />
         </View>
-        <View>
-            <View style={styles.progressBar}>
-                <View style={styles.absoluteFill} width={progressBarPercents[currentIndex]} />
-            </View>
-            <Text style={styles.questionNumText}>Question {currentIndex  + 1} of 5</Text>
+        <Text style={styles.questionNumText}>
+          Question {currentIndex + 1} of 5
+        </Text>
+      </View>
+      <View style={styles.questionView}>
+        <Card style={styles.questionCard}>
+          <Text style={styles.questionText}>
+            {questions[currentIndex].question}
+          </Text>
+        </Card>
+      </View>
+      <View>
+        <View style={styles.answerView}>
+          {questions[currentIndex].choices.map((choice, index) => {
+            return (
+              <TouchableOpacity onPress={() => saveChoice(index)} key={index}>
+                <Card
+                  style={
+                    questions[currentIndex].userChoice === index
+                      ? styles.answerChosenCard
+                      : styles.answerCard
+                  }
+                >
+                  {choice && (
+                    <Text
+                      style={
+                        questions[currentIndex].userChoice === index
+                          ? styles.cardChosenText
+                          : styles.cardText
+                      }
+                    >
+                      {choice}
+                    </Text>
+                  )}
+                </Card>
+              </TouchableOpacity>
+            )
+          })}
         </View>
-        <View style={styles.questionView}>
-            <Card style={styles.questionCard}>
-                <Text style={styles.questionText}>{questions[currentIndex].question}</Text>
-            </Card>
+        <View style={styles.buttonsView}>
+          {!currentIndex ? (
+            <TouchableOpacity
+              disabled={currentIndex === 4}
+              onPress={nextQuestion}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Next</Text>
+            </TouchableOpacity>
+          ) : currentIndex < 4 ? (
+            <>
+              <TouchableOpacity
+                disabled={!currentIndex}
+                onPress={previousQuestion}
+                style={styles.backButton}
+              >
+                <Text style={styles.backButtonText}>Back</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                disabled={currentIndex === 4}
+                onPress={nextQuestion}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>Next</Text>
+              </TouchableOpacity>
+            </>
+          ) : (
+            <>
+              <TouchableOpacity
+                disabled={!currentIndex}
+                onPress={previousQuestion}
+                style={styles.backButton}
+              >
+                <Text style={styles.backButtonText}>Back</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                disabled={currentIndex < 4}
+                onPress={onSubmit}
+                style={styles.button}
+              >
+                <Text style={styles.buttonText}>Submit</Text>
+              </TouchableOpacity>
+            </>
+          )}
         </View>
-        <View>
-            <View style={styles.answerView}>
-                {questions[currentIndex].choices.map((choice, index) => {
-                    return(
-                        <TouchableOpacity onPress={() => saveChoice(index)} key={index}>
-                            <Card style={chosenAnswer === index ? styles.answerChosenCard : styles.answerCard}>
-                                <Text style={chosenAnswer === index ? styles.cardChosenText : styles.cardText}>{choice.choiceText}</Text>
-                            </Card>
-                        </TouchableOpacity>
-                    )
-                })}
-            </View>
-            <View style={styles.buttonsView}>
-                {(!currentIndex) ?
-                    <TouchableOpacity disabled={currentIndex === 4} onPress={nextQuestion} style={styles.button}>
-                        <Text style={styles.buttonText}>Next</Text>
-                    </TouchableOpacity>
-                : (currentIndex < 4) ?
-                    <>
-                        <TouchableOpacity disabled={!currentIndex} onPress={previousQuestion} style={styles.backButton}>
-                            <Text style={styles.backButtonText}>Back</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity disabled={currentIndex === 4} onPress={nextQuestion} style={styles.button}>
-                            <Text style={styles.buttonText}>Next</Text>
-                        </TouchableOpacity>
-                    </>
-                :
-                    <>
-                        <TouchableOpacity disabled={!currentIndex} onPress={previousQuestion} style={styles.backButton}>
-                            <Text style={styles.backButtonText}>Back</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity disabled={currentIndex < 4} onPress={onSubmit} style={styles.button}>
-                            <Text style={styles.buttonText}>Submit</Text>
-                        </TouchableOpacity>
-                    </>
-                }
-            </View>
-        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F4F4F4'
-    },
-    header: {
-        backgroundColor: 'white',
-        height: '15%'
-    },
-    headerText: {
-        fontSize: 22,
-        fontWeight: 'bold',
-        color: '#0064A4',
-        textAlign: 'left',
-        marginTop: 70,
-        marginLeft: 20,
-    },
-    progressBar: {
-        height: 20,
-        width: '90%',
-        alignSelf: 'center',
-        backgroundColor: 'white',
-        marginTop: 20,
-        borderRadius: 10
-    },
-    absoluteFill: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        bottom: 0,
-        backgroundColor: "#94C83D",
-        borderRadius: 10,
-    },
-    questionNumText: {
-        fontSize: 16,
-        marginTop: 5,
-        marginLeft: 20
-    },
-    questionView: {
-        marginVertical: 15,
-        height: '23%',
-        width: '90%',
-        alignSelf: 'center',
-    },
-    questionCard: {
-        width: '98%',
-        height: '100%',
-        justifyContent: 'center',
-        paddingLeft: 20,
-        paddingRight: 20,
-    },
-    questionText: {
-        fontSize: 20,
-        color: '#555759',
-        textAlign: 'left'
-    },
-    cardText: {
-        fontSize: 18,
-        color: '#555759',
-        textAlign: 'center'
-    },
-    cardChosenText: {
-        fontSize: 18,
-        color: 'white',
-        textAlign: 'center'
-    },
-    answerCard: {
-        justifyContent: 'center',
-        alignSelf: 'center',
-        width: 160,
-        height: 100,
-    },
-    answerChosenCard: {
-        justifyContent: 'center',
-        alignSelf: 'center',
-        backgroundColor: '#6AA2B8',
-        width: 160,
-        height: 100,
-    },
-    answerView: {
-        marginBottom: 20,
-        justifyContent: 'space-around',
-        width: '89%',
-        height: '50%',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignSelf: 'center'
-    },
-    buttonsView: {
-        justifyContent: 'space-around',
-        width: '89%',
-        height: '10%',
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignSelf: 'center'
-    },
-    button: {
-        alignSelf: 'center',
-        width: '35%',
-        height: 40,
-        marginTop: 10,
-        marginBottom: 25,
-        backgroundColor: '#0064A4',
-        borderRadius: 20,
-        justifyContent: 'center'
-    },
-    backButton: {
-        alignSelf: 'center',
-        width: '35%',
-        height: 40,
-        marginTop: 10,
-        marginBottom: 25,
-        backgroundColor: '#F4F4F4',
-        justifyContent: 'center',
-        borderWidth: 1,
-        borderRadius: 20,
-        borderColor: '#555759'
-    },
-    buttonText: {
-        color: 'white',
-        fontSize: 17,
-        textTransform: 'capitalize',
-        alignSelf: 'center'
-    },
-    backButtonText: {
-        color: '#555759',
-        fontSize: 17,
-        textTransform: 'capitalize',
-        alignSelf: 'center'
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F4F4',
+  },
+  header: {
+    backgroundColor: 'white',
+    height: '15%',
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#0064A4',
+    textAlign: 'left',
+    marginTop: 70,
+    marginLeft: 20,
+  },
+  progressBar: {
+    height: 20,
+    width: '90%',
+    alignSelf: 'center',
+    backgroundColor: 'white',
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  absoluteFill: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    backgroundColor: '#94C83D',
+    borderRadius: 10,
+  },
+  questionNumText: {
+    fontSize: 16,
+    marginTop: 5,
+    marginLeft: 20,
+  },
+  questionView: {
+    marginVertical: 15,
+    height: '23%',
+    width: '90%',
+    alignSelf: 'center',
+  },
+  questionCard: {
+    width: '98%',
+    height: '100%',
+    justifyContent: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  questionText: {
+    fontSize: 20,
+    color: '#555759',
+    textAlign: 'left',
+  },
+  cardText: {
+    fontSize: 18,
+    color: '#555759',
+    textAlign: 'center',
+  },
+  cardChosenText: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+  },
+  answerCard: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    width: 160,
+    height: 100,
+  },
+  answerChosenCard: {
+    justifyContent: 'center',
+    alignSelf: 'center',
+    backgroundColor: '#6AA2B8',
+    width: 160,
+    height: 100,
+  },
+  answerView: {
+    marginBottom: 20,
+    justifyContent: 'space-around',
+    width: '89%',
+    height: '50%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignSelf: 'center',
+  },
+  buttonsView: {
+    justifyContent: 'space-around',
+    width: '89%',
+    height: '10%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignSelf: 'center',
+  },
+  button: {
+    alignSelf: 'center',
+    width: '35%',
+    height: 40,
+    marginTop: 10,
+    marginBottom: 25,
+    backgroundColor: '#0064A4',
+    borderRadius: 20,
+    justifyContent: 'center',
+  },
+  backButton: {
+    alignSelf: 'center',
+    width: '35%',
+    height: 40,
+    marginTop: 10,
+    marginBottom: 25,
+    backgroundColor: '#F4F4F4',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: '#555759',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 17,
+    textTransform: 'capitalize',
+    alignSelf: 'center',
+  },
+  backButtonText: {
+    color: '#555759',
+    fontSize: 17,
+    textTransform: 'capitalize',
+    alignSelf: 'center',
+  },
 })
 
 export default QuizForm
--- screens/Trivia/QuizPage.js
+++ screens/Trivia/QuizPage.js
@@ -32,133 +32,31 @@ import QuizForm from './QuizForm'
 
 LogBox.ignoreLogs(['Setting a timer'])
 
-const QuizPage = (props) => {
+const QuizPage = () => {
   const [questions, setQuestions] = useState([])
   const [currentIndex, setCurrentIndex] = useState(0)
-  const [chosenAnswer, setChosenAnswer] = useState(4)
-
-  const fetchChoice = async (docRefs) => {
-    try {
-      let choices = []
-      for (let i = 0; i < docRefs.length; i++) {
-        const docSnapShot = await docRefs[i].get()
-        choices.push(docSnapShot.data())
-      }
-      return choices
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const formatQuestions = async (questionsRef, questionsArr) => {
-    questionsRef.forEach((doc) => {
-      questionsArr.push({
-        id: doc.id,
-        choices: doc.data().choices,
-        difficulty: doc.data().difficulty,
-        question: doc.data().question,
-      })
-    })
-    for (let i = 0; i < questionsArr.length; i++) {
-      let choices = await fetchChoice(questionsArr[i].choices)
-      questionsArr[i].choices = choices
-    }
-  }
-
-  const fetchQuestions = async () => {
-    try {
-      const easyArr = []
-      const medArr = []
-      const hardArr = []
-
-      const easyQuestionsRef = await firebase
-        .firestore()
-        .collection('question')
-        .where('difficulty', '==', 'EASY')
-        .limit(2)
-        .get()
-      await formatQuestions(easyQuestionsRef, easyArr)
-
-      const mediumQuestionsRef = await firebase
-        .firestore()
-        .collection('question')
-        .where('difficulty', '==', 'MEDIUM')
-        .limit(2)
-        .get()
-      await formatQuestions(mediumQuestionsRef, medArr)
-
-      const hardQuestionsRef = await firebase
-        .firestore()
-        .collection('question')
-        .where('difficulty', '==', 'HARD')
-        .limit(1)
-        .get()
-      await formatQuestions(hardQuestionsRef, hardArr)
-
-      setQuestions([...easyArr, ...medArr, ...hardArr])
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  //temp solution to select random questions
-
-  const formatAllQuestions = async (questionsRef, questionsArr, limit) => {
-    let questionSize = 0
-
-    questionsRef.forEach((doc) => {
-      questionSize = questionSize + 1
-      questionsArr.push({
-        id: doc.id,
-        choices: doc.data().choices,
-        difficulty: doc.data().difficulty,
-        question: doc.data().question,
-      })
-    })
-
-    //create set and add n unique questions to set
-    let indexSet = new Set()
-    while (indexSet.size < limit) {
-      let random = Math.floor(Math.random() * questionsArr.length) + 1
-      indexSet.add(random)
-    }
-
-    //change set to array and query the choices using the randomly selected indexes
-    let indexArr = Array.from(indexSet)
-    for (let i = 0; i < limit; i++) {
-      let choices = await fetchChoice(questionsArr[indexArr[i]].choices)
-      questionsArr[indexArr[i]].choices = choices
-    }
-  }
 
   const fetchAllQuestions = async () => {
     try {
-      const easyArr = []
-      const medArr = []
-      const hardArr = []
+      const questionArr = []
 
-      const easyQuestionsRef = await firebase
+      const questionRef = await firebase
         .firestore()
-        .collection('question')
-        .where('difficulty', '==', 'EASY')
+        .collection('questions')
         .get()
-      await formatAllQuestions(easyQuestionsRef, easyArr, 2)
 
-      const mediumQuestionsRef = await firebase
-        .firestore()
-        .collection('question')
-        .where('difficulty', '==', 'MEDIUM')
-        .get()
-      await formatAllQuestions(mediumQuestionsRef, medArr, 2)
-
-      const hardQuestionsRef = await firebase
-        .firestore()
-        .collection('question')
-        .where('difficulty', '==', 'HARD')
-        .get()
-      await formatAllQuestions(hardQuestionsRef, hardArr, 2)
-
-      setQuestions([...easyArr, ...medArr, ...hardArr])
+      while (questionArr.length < 5) {
+        let random = Math.floor(Math.random() * questionRef.size)
+        if (questionArr.indexOf(random) === -1) {
+          questionArr.push({
+            choices: questionRef.docs[random].data().choices,
+            difficulty: questionRef.docs[random].data().difficulty,
+            question: questionRef.docs[random].data().question,
+            correctAnswer: questionRef.docs[random].data().correctAnswer,
+          })
+        }
+      }
+      setQuestions(questionArr)
     } catch (err) {
       console.error(err)
     }
@@ -169,7 +67,6 @@ const QuizPage = (props) => {
     if (0 <= currentIndex < questions.length) {
       setCurrentIndex(currentIndex + 1)
     }
-    setChosenAnswer(questions[currentIndex + 1].userChoice)
   }
 
   const previousQuestion = () => {
@@ -177,21 +74,16 @@ const QuizPage = (props) => {
     if (0 <= currentIndex < questions.length) {
       setCurrentIndex(currentIndex - 1)
     }
-    setChosenAnswer(questions[currentIndex - 1].userChoice)
   }
 
   const saveChoice = (choiceIndex) => {
     questions[currentIndex].userChoice = choiceIndex
     setQuestions([...questions])
-    setChosenAnswer(choiceIndex)
   }
 
   const calculateScore = () => {
     return questions.reduce((acc, question) => {
-      if (
-        question.userChoice &&
-        question.choices[question.userChoice].isAnswer
-      ) {
+      if (question.choices[question.userChoice] === question.correctAnswer) {
         return acc + 1
       } else {
         return acc
@@ -204,7 +96,6 @@ const QuizPage = (props) => {
     let userId = await SecureStore.getItemAsync('uid')
     let doc = await firebase.firestore().collection('users').doc(userId).get()
     if (!doc.exists) {
-      console.log(doc.data())
       alert('No user data found!')
     } else {
       let dataObj = doc.data()
@@ -215,13 +106,15 @@ const QuizPage = (props) => {
         .set({
           ...dataObj,
           points: dataObj.points + correct,
+          //showQuiz: 1, //uncomment after testing
         })
     }
   }
 
   useEffect(() => {
     //fetch on first render
-    fetchQuestions()
+    //fetchQuestions()
+    fetchAllQuestions()
   }, [])
 
   return (
@@ -234,7 +127,6 @@ const QuizPage = (props) => {
           previousQuestion={previousQuestion}
           saveChoice={saveChoice}
           onSubmit={onSubmit}
-          chosenAnswer={chosenAnswer}
         />
       ) : (
         <Text>Loading Questions</Text>
